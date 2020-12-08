221c19d6493721971f1d601941198db7aabcd90c
Hsin Hui <hsin.hui1118@gmail.com>
2020-12-08 16:05:24 +0800

    myproject

---
+++ src/com/fju/Poker.java
@@ -0,0 +1,10 @@
+package com.fju;
+
+public class Poker {
+
+	public static void main(String[] args) {
+    PokerCard card = new PokerCard(28);
+    System.out.println(card.value);
+	}
+
+}
---
+++ src/com/fju/PokerCard.java
@@ -0,0 +1,12 @@
+package com.fju;
+
+public class PokerCard {
+	 int value;
+	    char type;
+	    int point;
+	public PokerCard(int value) {
+	this.value = value;
+   
+	}
+
+}
